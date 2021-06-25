7ef03cfb89896ae33eaa110b86a5bac45b4b275f
igaki <github@musictale.net>
2021-06-25 15:57:35 +0900

    Playerクラスとdrawmonster実装

---
+++ Player.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+/**
+ * プレイヤーはモンスターデッキを持つ
+ *
+ * Deck：MonsterのArrayList
+ */
+public class Player {
+  ArrayList<Monster> deck = new ArrayList<>();
+
+  public void drawMonsters() {
+    for (int i = 0; i < 5; i++) {
+      this.deck.add(new Monster());
+    }
+  }
+}
